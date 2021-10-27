434add2a2cec9ab69afce5b9dbd249218196c593
Bogdan Zaranik <zaranikbz@gmail.com>
2021-10-27 13:10:38 +0300

    removed || 5000 from PORT on server

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ class Mailer {
 	}
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 // Have Node serve the files for our built React app
